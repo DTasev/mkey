540ee81d421e907cf0a3919d691e7a5d9ae9f321
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2018-10-19 20:05:59 +0100

    Now with parallel requests to blizz

--- mkey/leaderboard_viewer/views.py
+++ mkey/leaderboard_viewer/views.py
@@ -2,9 +2,13 @@
 import json
 import re
 
+import asyncio
+from functools import partial
+
 import requests
 from django.shortcuts import render
 from django.views import generic
+from multiprocessing.pool import Pool
 
 ACCESS_TOKEN_STRING = "access_token=myf2ucxu754w7u3zvu7extk3"
 GET_REALM_INFO_URL = "https://eu.api.battle.net/data/wow/realm/{0}?namespace=dynamic-eu&locale=en_GB&access_token=myf2ucxu754w7u3zvu7extk3"
@@ -18,13 +22,14 @@ class IndexView(generic.View):
     def post(self, request):
         try:
             realm_lowest_keys = get_realm_lowest_keys(request.POST["dungeon"], request.POST["wow_input"])
-            error_message=""
+            error_message = ""
         except:
-            error_message="Encountered an error, please make sure the names are in format [..] Name-Realm"
-            realm_lowest_keys=None
+            error_message = "Encountered an error, please make sure the names are in format [..] Name-Realm"
+            realm_lowest_keys = None
 
         return render(request, 'leaderboard_viewer/index.html',
-                      {"previous_player_input": request.POST["wow_input"], "realm_lowest_keys": realm_lowest_keys, "dungeon_name":request.POST["dungeon"], "error_message":error_message})
+                      {"previous_player_input": request.POST["wow_input"], "realm_lowest_keys": realm_lowest_keys,
+                       "dungeon_name": request.POST["dungeon"], "error_message": error_message})
 
 
 class PlayerData:
@@ -69,9 +74,8 @@ def get_realm_lowest_keys(dungeon, player_wow_input):
         player_wow_input = player_wow_input.split("\n")
     players = []
     for player_input in player_wow_input:
-
         if "] " in player_input:
-            # handles input copied from the game, this will be in the format of 
+            # handles input copied from the game, this will be in the format of
             # [time] <Name>-<Realm>, and splits the input into [name, realm] for each player
             player_and_realm = player_input.split("] ")[1].strip().split("-")
         elif "-" in player_input:
@@ -85,33 +89,47 @@ def get_realm_lowest_keys(dungeon, player_wow_input):
         player_data = PlayerData(*player_and_realm)
         players.append(player_data)
 
+    p = Pool(min(5, len(player_wow_input)))
+
+    # Create a queue that we will use to store our "workload".
+    realm_data_requests = partial(retrieve_realm_data, dungeon_id)
+
+    result = p.imap(realm_data_requests, players)
+
+    p.close()
+    p.join()
+
     realms_lowest_keys = []
-    for player in players:
-        response = requests.get(GET_REALM_INFO_URL.format(player.realm))
-        realm_info_json = json.loads(response.text)
-        # +1 moves the index in front of the slash
-        connected_id_start = realm_info_json["connected_realm"]["href"].rfind(
-            "/") + 1
-        connected_id_end = realm_info_json["connected_realm"]["href"].rfind("?")
-        connected_id = realm_info_json["connected_realm"]["href"][
-                       connected_id_start:connected_id_end]
-        print("Connected realm id", connected_id)
+    for r in result:
+        realms_lowest_keys.append(r)
 
-        mythic_leaderboard_response = requests.get(
-            GET_MYTHIC_LEADERBOARD_URL.format(connected_id))
-        mythic_leaderboard_json = json.loads(mythic_leaderboard_response.text)
-
-        for leaderboard in mythic_leaderboard_json["current_leaderboards"]:
-            if leaderboard["id"] == dungeon_id:
-                leaderboard_response = requests.get(leaderboard["key"]["href"] +
-                                                    "&" + ACCESS_TOKEN_STRING)
-                leaderboard_json = json.loads(leaderboard_response.text)
-                last_group = leaderboard_json["leading_groups"][-1]
-                seconds = (last_group["duration"] / 1000) % 60
-                minutes = (last_group["duration"] / (1000 * 60)) % 60
-                print("Last group level", last_group["keystone_level"], "Duration",
-                      "{0}m {1}s".format(minutes, seconds))
-                realms_lowest_keys.append(
-                    [str(player), last_group["keystone_level"], "{0}m {1}s".format(int(minutes), int(seconds))])
-                break
+    # sort by key level
     return sorted(realms_lowest_keys, key=lambda x: x[1])
+
+
+def retrieve_realm_data(dungeon_id, player):
+    response = requests.get(GET_REALM_INFO_URL.format(player.realm))
+    realm_info_json = json.loads(response.text)
+    # +1 moves the index in front of the slash
+    connected_id_start = realm_info_json["connected_realm"]["href"].rfind(
+        "/") + 1
+    connected_id_end = realm_info_json["connected_realm"]["href"].rfind("?")
+    connected_id = realm_info_json["connected_realm"]["href"][
+                   connected_id_start:connected_id_end]
+    print("Connected realm id", connected_id)
+
+    mythic_leaderboard_response = requests.get(
+        GET_MYTHIC_LEADERBOARD_URL.format(connected_id))
+    mythic_leaderboard_json = json.loads(mythic_leaderboard_response.text)
+
+    for leaderboard in mythic_leaderboard_json["current_leaderboards"]:
+        if leaderboard["id"] == dungeon_id:
+            leaderboard_response = requests.get(leaderboard["key"]["href"] +
+                                                "&" + ACCESS_TOKEN_STRING)
+            leaderboard_json = json.loads(leaderboard_response.text)
+            last_group = leaderboard_json["leading_groups"][-1]
+            seconds = (last_group["duration"] / 1000) % 60
+            minutes = (last_group["duration"] / (1000 * 60)) % 60
+            print("Last group level", last_group["keystone_level"], "Duration",
+                  "{0}m {1}s".format(minutes, seconds))
+            return [str(player), last_group["keystone_level"], "{0}m {1}s".format(int(minutes), int(seconds))]
